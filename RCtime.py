943904258b59557750abf1b7bf31e8418ac1ee9a
Remco <Remco.boys@gmail.com>
2016-04-04 21:30:00 +0200

    updated RCtime.py file

--- RCtime.py
+++ RCtime.py
@@ -1,16 +1,15 @@
-import RPi.GPIO as GPIO, time, os      
- 
-DEBUG = 1
+import RPi.GPIO as GPIO, time    
+
 GPIO.setmode(GPIO.BCM)
  
 def RCtime (RCpin):
-        reading = 0
-        GPIO.setup(RCpin, GPIO.OUT)
-        GPIO.output(RCpin, GPIO.LOW)
-        time.sleep(0.1)
- 
-        GPIO.setup(RCpin, GPIO.IN)
-        # This takes about 1 millisecond per loop cycle
-        while (GPIO.input(RCpin) == GPIO.LOW):
-                reading += 1
-        return reading
+	reading = 0
+	GPIO.setup(RCpin, GPIO.OUT)
+	GPIO.output(RCpin, GPIO.LOW)
+	time.sleep(0.1)
+
+	GPIO.setup(RCpin, GPIO.IN)
+	# This takes about 1 millisecond per loop cycle
+	while (GPIO.input(RCpin) == GPIO.LOW):
+			reading += 1
+	return reading
