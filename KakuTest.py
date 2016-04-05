5636df37e6210a6df5656985721e3a6322a1d25e
worms618 <remco.boys@gmail.com>
2016-04-05 08:48:59 +0000

    updated kakutest.py with external command and loop that turns the kaku on and off

--- KakuTest.py
+++ KakuTest.py
@@ -1,5 +1,19 @@
 
-import subprocess
-p = subprocess.Popen("./kaku 18 C on", cwd=/home/pi/wiringPi/examples/lights);
+import subprocess,time
+p = subprocess.Popen("./kaku 18 C on",shell = True, cwd="/home/pi/wiringPi/examples/lights");
 p.wait()
+
+def KakuSwitch(onoff):
+	if(onoff == True):
+		p = subprocess.Popen("./kaku 18 C on",shell= True, cwd="/home/pi/wiringPi/examples/lights");
+		print "Kaku on"
+	else:
+		p = subprocess.Popen("./kaku 18 C off",shell= True, cwd="/home/pi/wiringPi/examples/lights");
+		print "Kaku off"
+
+while True:
+	KakuSwitch(True)
+	time.sleep(5)
+	KakuSwitch(False)
+	time.sleep(5)
 
